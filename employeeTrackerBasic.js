c1eedf709329502fbf24474babb23ac30aca9716
Mario Cabrera <yankeescab@gmail.com>
2021-05-08 21:01:35 -0400

    adding table relationships

--- employeeTrackerBasic.js
+++ employeeTrackerBasic.js
@@ -2,7 +2,11 @@
 
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-
+const RawListPrompt = require("inquirer/lib/prompts/rawlist");
+const { printTable } = require("console-table-printer");
+const util = require("util");
+const { type } = require("os");
+const { title } = require("process");
 // conections
 
 const connection = mysql.createConnection({
@@ -22,19 +26,22 @@ const startQuestions = () => {
       type: `list`,
       message: `What would you like to do?`,
       choices: [
-        `Add to database`,
-        `Remove from database`,
-        `Update database`,
+        `Add to Database`,
+        `Remove from Database`,
+        `Update Database`,
+        `View Database`,
         `Nothing`,
       ],
     })
     .then((answer) => {
-      if (answer.introQuestion === "Add to database") {
+      if (answer.introQuestion === "Add to Database") {
         addToDb();
-      } else if (answer.introQuestion === "Remove from database") {
+      } else if (answer.introQuestion === "Remove from Database") {
         rmFromDb();
-      } else if (answer.introQuestion === "Update database") {
+      } else if (answer.introQuestion === "Update Database") {
         updateDb();
+      } else if (answer.introQuestion === "View Database") {
+        viewDb();
       } else {
         console.log("Have a good day!");
         connection.end;
@@ -94,7 +101,16 @@ const addDept = () => {
 
 // add a role
 
-const addRole = () => {
+const addRole = async () => {
+  connection.query = util.promisify(connection.query);
+  const dept = await connection.query(
+    "SELECT dept_name, dept_id FROM department"
+  );
+  console.log(dept);
+  const deptArray = dept.map(({ dept_name, dept_id }) => ({
+    name: title,
+    value: role_id,
+  }));
   inquirer
     .prompt([
       {
@@ -127,7 +143,14 @@ const addRole = () => {
 
 // add an employee
 
-const addEmployee = () => {
+const addEmployee = async () => {
+  connection.query = util.promisify(connection.query);
+  const roles = await connection.query("SELECT title, role_id FROM role");
+  console.log(roles);
+  const roleArray = roles.map(({ title, role_id }) => ({
+    name: title,
+    value: role_id,
+  }));
   inquirer
     .prompt([
       {
@@ -140,6 +163,12 @@ const addEmployee = () => {
         type: "input",
         message: "What is their last name?",
       },
+      {
+        name: "whatRole",
+        type: "list",
+        message: "What is their role?",
+        choices: roleArray,
+      },
     ])
     .then((answer) => {
       connection.query(
@@ -147,6 +176,7 @@ const addEmployee = () => {
         {
           first_name: answer.firstName,
           last_name: answer.lastName,
+          role_id: answer.whatRole,
         },
         (err) => {
           if (err) throw err;
@@ -159,13 +189,192 @@ const addEmployee = () => {
 
 // questions for removing from database
 
-const rmFromDb () => {
+const rmFromDb = () => {
   inquirer
-  .prompt({
-
-  })
+    .prompt({
+      name: "whatToRemove",
+      type: "list",
+      message: "What would you like to remove?",
+      choices: [`Department`, `Role`, `Employee`, `Nothing`],
+    })
+    .then((answer) => {
+      if (answer.whatToRemove === "Department") {
+        rmDept();
+      } else if (answer.whatToRemove === "Role") {
+        rmRole();
+      } else if (answer.whatToRemove === "Employee") {
+        rmEmployee();
+      } else {
+        console.log("Have a good day!");
+        connection.end;
+      }
+    });
 };
 
+// Remove a department
+
+const rmDept = () => {
+  connection.query("SELECT * FROM department", (err, results) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "selectDept",
+          type: "rawlist",
+          choices() {
+            choiceList = [];
+            results.forEach(({ dept_name }) => {
+              choiceList.push(dept_name);
+            });
+            choiceList.push("Go Back");
+            return choiceList;
+          },
+          message: "What department would you like to remove?",
+        },
+      ])
+      .then((answer) => {
+        if (answer.selectDept === "Go Back") {
+          startQuestions();
+        } else {
+          connection.query("DELETE FROM department WHERE ?", {
+            dept_name: answer.selectDept,
+          });
+          console.log("Department removed");
+          startQuestions();
+        }
+      });
+  });
+};
+
+// Remove a role
+
+const rmRole = () => {
+  connection.query("SELECT * FROM role", (err, results) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "selectRole",
+          type: "rawlist",
+          choices() {
+            choiceList = [];
+            results.forEach(({ title }) => {
+              choiceList.push(title);
+            });
+            choiceList.push("Go Back");
+            return choiceList;
+          },
+          message: "What role would you like to remove?",
+        },
+      ])
+      .then((answer) => {
+        if (answer.selectRole === "Go Back") {
+          startQuestions();
+        } else {
+          connection.query("DELETE FROM role WHERE ?", {
+            title: answer.selectRole,
+          });
+          console.log("Role removed");
+          startQuestions();
+        }
+      });
+  });
+};
+
+// Remove an Employee
+
+const rmEmployee = () => {
+  connection.query("SELECT * FROM employee", (err, results) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "selectEmployee",
+          type: "rawlist",
+          choices() {
+            choiceList = [];
+            results.forEach(({ first_name }) => {
+              choiceList.push(first_name);
+            });
+            choiceList.push("Go Back");
+            return choiceList;
+          },
+          message: "Which employee would you like to remove?",
+        },
+      ])
+      .then((answer) => {
+        if (answer.selectEmployee === "Go Back") {
+          startQuestions();
+        } else {
+          connection.query("DELETE FROM employee WHERE ?", {
+            first_name: answer.selectEmployee,
+          });
+          console.log("Employee removed");
+          startQuestions();
+        }
+      });
+  });
+};
+
+// Viewing Database
+
+const viewDb = () => {
+  connection.query = util.promisify(connection.query);
+  inquirer
+    .prompt({
+      name: "viewWhat",
+      type: "list",
+      message: "What table do you want to view?",
+      choices: ["Department", "Role", "Employee", "View Company", "Nevermind"],
+    })
+    .then((answer) => {
+      switch (answer.viewWhat) {
+        case "Department":
+          viewDept();
+          break;
+        case "Role":
+          viewRole();
+          break;
+        case "Employee":
+          viewEmployee();
+          break;
+        case "View Company":
+          // call view company
+          break;
+        default:
+          startQuestions();
+          break;
+      }
+    });
+};
+const viewDept = () => {
+  connection.query = util.promisify(connection.query);
+  connection
+    .query("SELECT dept_name AS Department FROM department")
+    .then((res) => printTable(res))
+    .catch((err) => console.log(err));
+};
+const viewRole = () => {
+  connection.query = util.promisify(connection.query);
+  connection
+    .query("SELECT title AS Title FROM role")
+    .then((res) => printTable(res))
+    .catch((err) => console.log(err));
+};
+const viewEmployee = () => {
+  connection.query = util.promisify(connection.query);
+  connection
+    .query("SELECT CONCAT(first_name, ' ', last_name) AS Name FROM employee")
+    .then((res) => printTable(res))
+    .catch((err) => console.log(err));
+};
+// const viewCompany = () => {
+//   connection.query = util.promisify(connection.query);
+//   connection
+//     .query("SELECT dept_name AS Department FROM department")
+//     .then((res) => printTable(res))
+//     .catch((err) => console.log(err));
+// };
 //start server
 
 connection.connect((err) => {
